226de0c6bb9d6efea410160a5dc95953e8417da9
Afaq <afaqnaveed@gmail.com>
2025-07-04 19:32:23 +0500

    Switched to Modular Programming- Implemented API fetch logic with error handling in file api.js

---
+++ api.js
@@ -0,0 +1,23 @@
+const BASE_URL = "https://www.dragonball-api.com/api";
+
+async function request(endpoint, { method = "GET", body = null } = {}) {
+  const res = await fetch(`${BASE_URL}/${endpoint}`, {
+    method,
+    headers: { "Content-Type": "application/json"},
+    ...(body && { body: JSON.stringify(body) })
+  });
+  if (!res.ok) {
+    const err = await res.json().catch(() => ({}));
+    throw new Error(err.message || res.statusText);
+  }
+  return res.json();
+}
+
+export function getCharacters() {
+  console.log(request("characters"));
+  return request("characters");
+}
+
+export function getCharacterById(id) {
+  return request(`characters/${id}`);
+}
